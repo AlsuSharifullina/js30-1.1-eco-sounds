f93f7abfc7fde55c7106ae2ab249dca0002990d3
Alsu Sharifullina <sharifullina1998@inbox.ru>
2024-09-22 23:30:30 +0300

    init: add audio, chahge styles

---
+++ js30-1.1/script.js
@@ -0,0 +1,4 @@
+const player = document.querySelector('."player'),
+      playBtn = document.querySelector('.play'),
+      prevBtn = document.querySelector('.prev'),
+      nextBtn = document.querySelector('.next'),
